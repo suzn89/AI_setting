ac730c5c91b2b146794b5fdd25d5dac5fce847ba
suzn89 <ssostory@gmail.com>
2019-04-01 14:16:08 +0900

    사용자 리스트화면 완료

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('#popup__wrap').hide();
+  $('#view_popup').on('click',function(){
+    $('#popup__wrap').fadeIn();
+  });
+  $('#popup__wrap .popup_close').on('click',function(){
+    $('#popup__wrap').hide();
+  });  
+  $('.popup__bottom .close').on('click',function(){
+    $('#popup__wrap').hide();
+  });  
+});
