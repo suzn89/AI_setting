111ee300c47e9f4c7b3e1dd880996bcc2e3df302
suzn89 <ssostory@gmail.com>
2019-04-02 14:02:12 +0900

    채널그룹관리 완료

--- js/common.js
+++ js/common.js
@@ -12,4 +12,21 @@ $(function(){
   $('#view_roi').on('click',function(){
     $('#popup__wrap').fadeIn();
   });
+
+  // 채널목록 그리드
+  $('.btn__add').on('click',function(){
+    $('#popup__wrap').fadeIn();
+  });
+
+  // 채널 그룹 관리 수정,삭제
+  $('.box__list--hover').hide();
+  $('.infotbl').hover(function () {
+      // over
+      $('.box__list--hover').show();
+    }, function () {
+      // out
+      $('.box__list--hover').hide();
+    }
+  );
+
 });
